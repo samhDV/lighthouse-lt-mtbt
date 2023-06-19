c934ac78f674bb09d8376716da67d26703c760e0
samhDV <97615662+samhDV@users.noreply.github.com>
2023-06-19 12:03:54 +0000

    fix

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
-function syncLoop(amount){
-    console.log('start sync loop ' +amount)
+function syncLoop(amount) {
+    console.log('start sync loop ' + amount)
     start = Date.now();
-    while (Date.now() < start + amount){}
-    console.log('end sync loop ' +amount)
-   
-  }
-  
-  setTimeout(syncLoop.bind(this,300),0)
-  setTimeout(syncLoop.bind(this,400),1000)
-  setTimeout(syncLoop.bind(this,500),2000)
-  
-  
+    while (Date.now() < start + amount) { }
+    console.log('end sync loop ' + amount)
+
+}
+
+setTimeout(syncLoop.bind(this, 250), 100)
+
